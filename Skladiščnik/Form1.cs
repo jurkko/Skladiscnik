0e19f9f10afe5598a0d7666fb8e1ec1d784745f1
jurkko123 <jure.turk@student.um.si>
2018-12-19 14:42:09 +0100

    login design

---
+++ Skladiščnik/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Skladiščnik
+{
+    public partial class Form1 : Form
+    {
+
+    
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+            textBox2.PasswordChar = '*';
+            pictureBox3.BackgroundImage = Properties.Resources.geslo1;
+            panel2.ForeColor = Color.FromArgb(78, 184, 206);
+            textBox1.ForeColor = Color.FromArgb(78, 184, 206);
+
+            pictureBox2.BackgroundImage = Properties.Resources.uporabnik2;
+            panel2.ForeColor = Color.WhiteSmoke;
+            textBox1.ForeColor = Color.WhiteSmoke;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            pictureBox2.BackgroundImage = Properties.Resources.uporabnik2;
+            panel1.ForeColor = Color.FromArgb(78, 184, 206);
+            textBox1.ForeColor = Color.FromArgb(78, 184, 206);
+
+            pictureBox2.BackgroundImage = Properties.Resources.uporabnik1;
+            panel1.ForeColor = Color.WhiteSmoke;
+            textBox1.ForeColor = Color.WhiteSmoke;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+        Point lastPoint;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+                Application.Exit();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
